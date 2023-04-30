b3cd9af29980a574c0958bf8fdef558f8af6ac94
sasholilov <alex_xld@abv.bg>
2023-04-30 20:21:38 +0300

    Shoppping Cart

---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+
+import { ProductsContext } from "../../contexts/products.context";
+
+import ProductCard from "../../components/product-card/product-card.component";
+import "./shop.styles.scss";
+
+const Shop = () => {
+  const { products } = useContext(ProductsContext);
+  return (
+    <div className="products-container">
+      {products.map((product) => (
+        <div key={product.id}>
+          <ProductCard product={product} />
+          <h1>{product.name}</h1>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Shop;
