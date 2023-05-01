012283a0ab35535490b2a8562c9bdbd0c7b7e6b4
sasholilov <alex_xld@abv.bg>
2023-05-01 23:00:30 +0300

    add to cart feature

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,14 +7,20 @@ import { CartContext } from "../../contexts/cart.context";
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
+
+  // const itemsCount = cartItems.reduce((acc, item) => {
+  //   const count = (acc += item.quantity);
+  //   return count;
+  // }, 0);
 
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import "./product-card.styles.scss";
 import Button from "../button/button.component";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+  const addProducttoCart = () => addItemToCart(product);
 
   return (
     <div className="product-card-container">
@@ -11,7 +15,9 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to card</Button>
+      <Button buttonType="inverted" onClick={addProducttoCart}>
+        Add to card
+      </Button>
     </div>
   );
 };
